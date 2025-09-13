3bee4240c1864e6138239660f5ce3b5e3229b51f
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-09-12 19:30:11 -0600

    agregar dato minuto entrada cola

--- graficagraphviz.py
+++ graficagraphviz.py
@@ -57,9 +57,10 @@ node [shape=box, style=filled, fillcolor=lightyellow, fontname="Helvetica"];
         edad = paciente.info.edad   
         especialidadmedica = paciente.info.especialidadmedica
         minutoentredaatendido = paciente.info.minutoentredaatendido 
+        minutoentradaalacola = paciente.info.minutoentradaalacola
         print(f"Paciente: {nombre}, {edad}, {especialidadmedica}, {minutoentredaatendido}")
-        dot_text += f'paciente{i} [label="Nombre: {nombre}\nEdad: {edad}\nEspecialidad Medica: {especialidadmedica}\nMinuto atendido: {minutoentredaatendido}"];\n'
-        
+        dot_text += f'paciente{i} [label="Nombre: {nombre}\nEdad: {edad}\nEspecialidad Medica: {especialidadmedica}\nMinuto entrada: {minutoentradaalacola}\nMinuto atendido: {minutoentredaatendido}"];\n'
+
     print("\n" + "/"*10 + "Fin Recorrer Cola" + "/"*10)
 
 
